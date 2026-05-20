97c31efb8823db0686e767bffcc4b00ffb73d043
eshan <muhammedeshan868@gmail.com>
2026-05-20 22:32:25 +0530

    initial commit

--- food_crave/food_crave/urls.py
+++ food_crave/food_crave/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for robobuild project.
+URL configuration for food_crave project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
