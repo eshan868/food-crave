5310d51dde9cf1722c3631fd4139045bd8a1c98c
eshan <muhammedeshan868@gmail.com>
2026-05-21 21:51:58 +0530

    base model creation

--- food_crave/accounts/models.py
+++ food_crave/accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('restaurant_owner', 'Restaurant_owner'),
+        ('delivery_man', 'Delivery_man'),
+    )
+
+    role=models.CharField(
+        max_length=100,
+        choices=ROLE_CHOICES
+    )
+
+    phone=models.CharField(max_length=15)
+    address=models.TextField()
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/',
+        blank=True,
+        null=True
+    )
+    def __str__(self):
+        return self.username
+    
