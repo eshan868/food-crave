b7d37e3eaf007bda37b12bc855fcbcd9e6619654
eshan <muhammedeshan868@gmail.com>
2026-06-13 22:14:28 +0530

    render deploy setup modified

--- food_crave/food_crave/settings.py
+++ food_crave/food_crave/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-)7$$%0^-x*)10su-14_+e6jy_fr&7^1-#)sw)0rd@%dcd&i=_h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+]
 
    
 # Application definition
