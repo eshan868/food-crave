c3228512878ba8306ff52918a814d55a8262cec2
eshan <muhammedeshan868@gmail.com>
2026-06-13 22:01:48 +0530

    modify admin.py

--- food_crave/orders/admin.py
+++ food_crave/orders/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-  
+from accounts.models import User
+from restaurants.models import Restaurant, food_items
+from orders.models import Order
+from delivery.models import DeliveryAssignment
+
+
+admin.site.register(User)
+admin.site.register(Restaurant)
+admin.site.register(food_items)
+admin.site.register(Order)
+admin.site.register(DeliveryAssignment)
